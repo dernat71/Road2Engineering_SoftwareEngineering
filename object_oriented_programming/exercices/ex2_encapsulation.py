7124f558039a200971478595441ebc825fc58824
Derave, Nathan <nathan.derave@accenture.com>
2020-07-28 00:30:36 +0200

    Modif ex encapsulation

--- object_oriented_programming/exercices/ex2_encapsulation.py
+++ object_oriented_programming/exercices/ex2_encapsulation.py
@@ -1,4 +1,4 @@
 # Take result of exercice 1 and 
-# * make one attribute as private
-# * try to print it (did you got the error ?)
-# * define a getter function to get the value of the private attribute (return self.__the_priv_attribute)
+# 1) make one attribute as private
+# 2) try to print it (did you got the error ?)
+# 3) define a getter function to get the value of the private attribute (return self.__the_priv_attribute)
