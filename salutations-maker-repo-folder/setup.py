2196422c1ae3087c802c0ee8ce17afb5cd848fd4
Derave, Nathan <nathan.derave@accenture.com>
2020-08-01 10:12:50 +0200

    PACKAGING 2 : READY TO BUILD (Fixed naming)

--- salutations-maker-repo-folder/setup.py
+++ salutations-maker-repo-folder/setup.py
@@ -10,7 +10,7 @@ with open('requirements.txt') as f:
         requirements.append(str(req).replace('==', '>='))
 
 setup(
-    name="salutations_maker", # Replace with your own username
+    name="salutations-maker", # Replace with your own username
     version="0.0.1",
     author="Nathan Derave",
     author_email="nathan.derave@accenture.com",
