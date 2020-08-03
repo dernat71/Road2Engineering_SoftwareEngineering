0d15ace17b4acb728db4414c603fbf44a65d4b22
Derave, Nathan <nathan.derave@accenture.com>
2020-08-03 23:32:27 +0200

    PACKAGING 4 : ADDED SETUP.CFG

--- salutations-maker-repo-folder/setup.py
+++ salutations-maker-repo-folder/setup.py
@@ -10,24 +10,5 @@ with open('requirements.txt') as f:
         requirements.append(str(req).replace('==', '>='))
 
 setup(
-    name="salutations-maker", # Replace with your own username
-    version="0.0.1",
-    author="Nathan Derave",
-    author_email="nathan.derave@accenture.com",
-    description="A package to say hello",
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-    package_dir={'': 'src'},
-    install_requires=requirements,
-    extras_require={
-        'dev': ['pytest', 'flake8', 'tox'],
-    }
-    
-)
+    install_requires=requirements)
